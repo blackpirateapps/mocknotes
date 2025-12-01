129739082cdc99c51ae154989076249035a398ab
blackpirateapps <github@blackpirateapps.com>
2025-12-02 00:19:24 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+// Pages
 import Dashboard from './pages/Dashboard';
 import Importer from './pages/Importer';
 import MockDetail from './pages/MockDetail';
 import Settings from './pages/Settings';
+import TakeQuiz from './pages/TakeQuiz'; // <--- Import the new page
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/add" element={<Importer />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/mock/:id" element={<MockDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/add" element={<Importer />} />
+      <Route path="/mock/:id" element={<MockDetail />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/take-quiz" element={<TakeQuiz />} /> {/* <--- Add this line */}
+    </Routes>
   );
 }
